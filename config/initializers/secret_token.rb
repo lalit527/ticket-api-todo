d154257f7ac30dbbccc71cc7a33a0dd37aa3aa6f
Lalit-Yadav02 <lalit.yadav@freshworks.com>
2018-02-22 18:53:14 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketApiTodo::Application.config.secret_token = 'be9f05c883912cabc0bfcc0e0e83f55af9efdc03bfd4b39cdb925b55a582408685763d16321e1675e4ab6e6ec222ba636c04e90d388f9ab5e3444d53932f58ff'
